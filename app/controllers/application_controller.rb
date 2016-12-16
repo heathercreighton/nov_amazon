842acc2d711b25e9541076156c84e6b936d97030
Heather Creighton <heather.creighton@gmail.com>
2016-12-15 22:23:50 -0500

    added full amazon

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+ 
+  before_filter :categories, :brands
+
+def configure_permitted_parameters
+
+  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, 
+     :password_confirmation, :role) }
+
+  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, 
+     :password_confirmation, :current_password, :role) }
+end
+
+
+  def categories
+  	@categories = Category.all
+  end
+  
+  def brands
+
+  	@brands = Product.pluck(:brand).sort.uniq!
+    
+
+  	if @brands == nil
+  		@brands = Product.pluck(:brand).sort
+  	end	
+
+  end	
+end
---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,66 @@
+class CartController < ApplicationController
+  
+	before_filter :authenticate_user!, :except => [:add_to_cart, :view_order]
+
+  def add_to_cart
+  	line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
+
+	  line_item.line_item_total = line_item.quantity * line_item.product.price
+	  line_item.save
+
+	  redirect_to root_path
+
+  end
+
+  def view_order
+
+  	@line_items = LineItem.all
+  end
+
+  def checkout
+
+  	@line_items = LineItem.all
+	  @order = Order.create(user_id: current_user.id, subtotal: 0)
+
+	  @line_items.each do |line_item|
+	    @order.order_items[line_item.product_id] = line_item.quantity
+	    @order.subtotal += line_item.line_item_total
+	  end
+
+	  @order.sales_tax = @order.subtotal * 0.07
+	  @order.grand_total = @order.subtotal + @order.sales_tax
+	  @order.save
+
+	  @line_items.each do |line_item|
+	    line_item.product.quantity -= line_item.quantity
+	    line_item.product.save
+	  end
+
+	  LineItem.destroy_all
+
+  end
+
+  def order_complete
+
+  	@order = Order.find(params[:order_id])
+    @amount = (@order.grand_total.to_f.round(2) * 100).to_i
+
+    customer = Stripe::Customer.create(
+      :email => current_user.email,
+      :card => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      :description => 'Rails Stripe customer',
+      :currency => 'usd'
+    )
+
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to cart_path
+
+
+  end	
+end
